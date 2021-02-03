e182426691dc4c5cd70ca02be639529fbd7025f1
MrTager <mrtager@qq.com>
2021-02-03 16:54:18 +0800

    解决antd样式不生效问题，编写登录页面

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -22,57 +22,59 @@ export default class Login extends React.Component {
         return (
             <div id='login-page' style={styles.page}>
                 <div className={'formContainer'}>
-                    <Form
-                        name="normal_login"
-                        className="login-form"
-                        initialValues={{
-                            remember: true,
-                        }}
-                        onFinish={this.onFinish}
-                    >
-                        <Form.Item
-                            name="username"
-                            rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Username!',
-                            },
-                            ]}
+                    <div className={"inputBox"}>
+                        <Form
+                            name="normal_login"
+                            className="login-form"
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={this.onFinish}
                         >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[
-                            {
-                                required: true,
-                                message: 'Please input your Password!',
-                            },
-                            ]}
-                        >
-                            <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                            placeholder="Password"
-                            />
-                        </Form.Item>
-                        
-                        <Form.Item name="remember" valuePropName="checked" noStyle>
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-                        <Form.Item>
-                            <a className="login-form-forgot" href="">
-                            Forgot password
-                            </a>
-                        </Form.Item>
+                            <Form.Item
+                                name="username"
+                                rules={[
+                                {
+                                    required: true,
+                                    message: '请输入用户名!',
+                                },
+                                ]}
+                            >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                            </Form.Item>
+                            <Form.Item
+                                name="password"
+                                rules={[
+                                {
+                                    required: true,
+                                    message: '请输入密码!',
+                                },
+                                ]}
+                            >
+                                <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="密码"
+                                />
+                            </Form.Item>
+                            
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>记住我</Checkbox>
+                            </Form.Item>
+                            <Form.Item>
+                                <a className="login-form-forgot" href="">
+                                忘记密码
+                                </a>
+                            </Form.Item>
 
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
-                            </Button>
-                            Or <a href="">register now!</a>
-                        </Form.Item>
-                    </Form>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                登录
+                                </Button>
+                                或者 <a href="">注册</a>
+                            </Form.Item>
+                        </Form>
+                    </div>
                 </div>
             </div>
         )
