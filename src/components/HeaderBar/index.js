e1982410177775c210fea19582f457f1d51cb99f
mrtager <mrtager@qq.com>
2021-02-10 18:51:41 +0800

    code headerBar screenfull

--- src/components/HeaderBar/index.js
+++ src/components/HeaderBar/index.js
@@ -3,8 +3,10 @@ import propTypes from 'prop-types';
 import { defaults } from 'js-cookie';
 import { connect } from 'react-redux';
 import { telescopicSldebar } from '../../store/HeaderBar/action'
-import { MenuFoldOutlined,MenuUnfoldOutlined } from '@ant-design/icons';
-import './index.css.map'
+import { MenuFoldOutlined,MenuUnfoldOutlined,ShrinkOutlined,ArrowsAltOutlined } from '@ant-design/icons';
+import screenfull from "screenfull";
+import { message } from 'antd';
+import './index.css'
 
 class HeaderBar extends Component {
     static propTypes = {
@@ -12,18 +14,37 @@ class HeaderBar extends Component {
         telescopicSldebar: propTypes.func.isRequired
     }
     state = {
-
+        iconSize:25,
+        screenFull:false
     }
     changeCollapsed = () => {
         this.props.telescopicSldebar('collapsed',this.props.collapsed)
+    }
+    changeScreenFull = () => {
+        if(screenfull.isEnabled){
+            screenfull.toggle()
+            this.setState({screenFull:!screenfull.isFullscreen})
+        }else{
+            message.error('您的浏览器不支持此功能！')
+        }
     }
     render() {
         return (
             <div id="header-bar">
                 <div className={'collapsed'} onClick={this.changeCollapsed}>
                     {
-                        this.props.collapsed ? (<MenuFoldOutlined style={{ fontSize: '25px' }}/>) : (<MenuUnfoldOutlined style={{ fontSize: '25px' }} />)
+                        this.props.collapsed ? (<MenuFoldOutlined style={{ fontSize: `${this.state.iconSize}px` }}/>) : (<MenuUnfoldOutlined style={{ fontSize: `${this.state.iconSize}px` }} />)
                     }
+                </div>
+                <div style={{lineHeight:'64px',float:'right'}}>
+                    <ul className={'header-ul'}>
+                        <li onClick={this.changeScreenFull}>
+                            {
+                                this.state.screenFull ? (<ShrinkOutlined style={{ fontSize: `${this.state.iconSize}px` }}/>) : (<ArrowsAltOutlined style={{ fontSize: `${this.state.iconSize}px` }}/>)
+                            }
+                        </li>
+                        <li></li>
+                    </ul>
                 </div>
             </div>
         )
